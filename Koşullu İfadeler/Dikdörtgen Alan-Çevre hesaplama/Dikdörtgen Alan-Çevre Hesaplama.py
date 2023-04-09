e76ba5587c7bd1d40ace77a5ebfe15a41e317faa
Burak Gür <burakyigitgur@ogr.iu.edu.tr>
2023-04-09 03:24:44 +0300

    Create Dikdörtgen Alan-Çevre Hesaplama.py

---
+++ Koşullu İfadeler/Dikdörtgen Alan-Çevre hesaplama/Dikdörtgen Alan-Çevre Hesaplama.py
@@ -0,0 +1,8 @@
+print("kısa Kenar Uzunluğunu Giriniz")
+kisaKenar = int(input())
+print("uzun Kenarı Giriniz")
+uzunKenar = int(input())
+cevre = 2 * (kisaKenar + uzunKenar)
+alan = kisaKenar * uzunKenar
+print("Çevre: " + str(cevre))
+print("Alan: " + str(alan))
